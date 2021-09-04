9c131e1835e3923d9ff1c406f325dfb6e98f0edb
lvming <lvming01@rd.netease.com>
2021-09-04 15:48:45 +0800

    修改721contract

---
+++ contracts/StarXERC721Factory.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./StarXERC721Token.sol";
+
+/**
+ * ERC721 factory
+ */
+contract StarXERC721TokenFactory is Ownable {
+    using Strings for string;
+
+    /**
+     * StarX token create event
+     */
+    event StarXTokenCreated(
+        address _owner,
+        address _token,
+        string name,
+        string sympol,
+        string baseTokenURI
+    );
+
+    //save the user's  all token address 
+    mapping(address => address[]) starXTokens;
+    
+    // save all tokens
+    address[] private allStarXTokens;
+
+    /**
+     * @dev user can get one StarX721 mintor for the method
+     */
+    function createStarX(string memory _name,string memory _sympol,string memory _baseTokenURI) external returns(address _token){
+        StarXERC721Token token = new StarXERC721Token(_name,_sympol,_baseTokenURI);
+
+        address starXToken = address(token);
+        starXTokens[msg.sender].push(starXToken);
+        allStarXTokens.push(starXToken);
+
+        emit StarXTokenCreated(msg.sender, starXToken, _name, _sympol, _baseTokenURI);
+        return starXToken;
+    }
+
+    /**
+     * @dev Get all StarXTokens for the _owner address
+     */
+    function getTokensByAddress(address _owner) external view returns(address[] memory){
+        return starXTokens[_owner];
+    }
+
+    /**
+     * @dev Get all StarXTokens
+     */
+    function getAllStarXTokens() external view returns(address[] memory){
+        return allStarXTokens;
+    }
+}
---
+++ contracts/StarXERC721Token.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+/**
+ * ERC721 Token 
+ */
+contract StarXERC721Token is ERC721Enumerable,Ownable,Pausable{
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+    
+    //the curTokenId
+    Counters.Counter private curTokenId;
+
+    //token URI prefix
+    string private baseTokenURI;
+    
+    //save token creators
+    mapping(uint256 => address) creators;
+    
+    constructor(string memory _name,string memory _sympol,string memory _baseTokenURI) ERC721(_name, _sympol){
+        baseTokenURI = _baseTokenURI;
+    }
+    /**
+     * @dev set one new base URI only by owner
+     * @param _newTokenURI a new base URI
+     */
+    function setBaseTokenURI(string memory _newTokenURI) public onlyOwner{
+        baseTokenURI = _newTokenURI;
+    }
+    /**
+     * @dev set base URI for ERC721
+     */
+    function _baseURI() internal view override returns (string memory) {
+        return baseTokenURI;
+    }
+    /**
+     * @dev return current base URI
+     */
+    function getbaseTokenURI() virtual public view returns(string memory){
+        return baseTokenURI;
+    }
+    /**
+     * @dev user can mint one 721 Token for _toAddress
+     * @param _toAddress owner of the ERC721 Token 
+     */
+    function mintTo(address _toAddress) public {
+        //id increment every time a mint
+        curTokenId.increment();
+        _safeMint(_toAddress,curTokenId.current());
+        creators[curTokenId.current()] = msg.sender;
+    }
+    /**
+     * @dev returns the curren token Id for the contract 
+     */
+    function getCurTokenId() public view returns(uint256){
+        return curTokenId.current();
+    }
+    
+    /**
+     * @dev returns current token URI for the contract
+     */
+    function getCurTokenURI() public view returns(string memory){
+        return tokenURI(curTokenId.current());
+    }
+    
+    /**
+     * @dev get token URI by _tokenId
+     */
+    function tokenURI(uint256 _tokenId) override public view returns(string memory){
+     return string(abi.encodePacked(baseTokenURI, _tokenId.toString()));
+  }
+
+  /**
+   * @dev exists _tokenId
+   */
+  function exists(uint256 _tokenId) public view returns(bool){
+      return _exists(_tokenId);
+  }
+
+  /**
+   * @dev pause the 721Token 
+   */
+  function pause() external onlyOwner{
+      _pause();
+  }
+
+  /**
+   * @dev unPause the 721Token 
+   */
+  function unPause() external onlyOwner{
+      _unpause();
+  }
+
+}
