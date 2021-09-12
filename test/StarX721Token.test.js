c5b2ed154bfd20eea25473a8462c266a736b0fc3
lvming <lvming01@rd.netease.com>
2021-09-12 21:49:43 +0800

    添加测试用例

--- test/StarX721Token.test.js
+++ test/StarX721Token.test.js
@@ -2,7 +2,7 @@ require("dotenv").config;
 const {time,expectRevert} = require('@openzeppelin/test-helpers');
 const { inTransaction } = require("@openzeppelin/test-helpers/src/expectEvent");
 const { assertion } = require("@openzeppelin/test-helpers/src/expectRevert");
-const { assert } = require("chai");
+const { assert, use } = require("chai");
 const StarX721Token  = artifacts.require("StarX721Token");
 const StarX721TokenFactory = artifacts.require("StarX721TokenFactory");
 
@@ -33,7 +33,7 @@ contract('StarX721Token',(accounts) =>{
         await starX721Token.mint(buyer,{from:buyer})
     })
 
-    describe('test StarX721factory', async()=>{
+    describe('Test StarX721factory', async()=>{
         it('1st,Get the owner',async()=>{
             const owner = await startX721Factory.owner
             assert.equal(owner,accounts[0])
@@ -44,5 +44,72 @@ contract('StarX721Token',(accounts) =>{
             firstToken = tokens[0]
             assert.equal(tokens.length,1)
         })
+        it('3st,Get token from address',async()=>{
+            const token = await startX721Factory.getTokensByAddress(owner)
+            assert.equal(token,firstToken)
+        })
     })
+    describe('Test StarX721 Token',async()=>{
+        it('1st test,test the info of the StarX721 token', async()=>{
+            assert.equal(await starXToken.name(),"test 721token")
+            assert.equal(await starXToken.symbol,"StarX")
+            let tokenId = starXToken.getCurTokenId()
+            assert.equal(await starXToken.tokenURI(tokenId),baseUR+tokenId)
+        })
+
+        it('2st test, get the totalSupply', async () => {
+            const total = await starX721Token.totalSupply()
+            assert.equal(total.toString(), '3')
+        })
+
+        it('3st test, approve other',async()=>{
+            await starXToken.approve(user,1,{from:owner})
+            const approved = await starXToken.getApproved(1)
+            assert.equal(user,approved)
+        })
+
+        it('4st test, transfer token to other',async()=>{
+            const balanceOfOwner = await starXToken.balanceOf(owner)
+            const balanceOfUser = await starXToken.balanceOf(user)
+            await starXToken.safeTransferFrom(owner,user,1,{from:owner})
+            const ownerBalance = await starXToken.balanceOf(owner)
+            const userBalance = await starXToken.balanceOf(user)
+            assert.equal(balanceOfOwner.sub(ownerBalance),'1')
+            assert.equal(userBalance.sub(balanceOfUser),'1')
+        })
+
+        it('5st test,the contract can  pause by owner',async()=>{
+            assert.isFalse(await starXToken.paused())
+            await expectRevert(starX721Token.pause({from:user}),"caller is not the owner")
+            await starX721Token.pause({from:owner})
+            assert.isTrue(await starX721Token.paused())
+        })
+
+        it('6st test, the contract can not mint when paused',async()=>{
+            await expectRevert(tarX721Token.mint(owner,{from:owner}),"contact is paused by owner,can not mint")
+            await starX721Token.unpause({from:owner})
+        })
+
+        it('7st test,set new baseURI',async()=>{
+            const newBaseURI = 'https://testapi.ipfs.cn/nft?id='
+            await starX721Token.setBaseTokenURI(newBaseURI, {from: owner})
+            let curTokenId = starX721Token.getCurTokenId()
+            assert.equal(await starX721Token.tokenURI(curTokenId), baseUR+tokenId)
+        })
+
+        it('8st test,get token owner of the tokenId',async()=>{
+            const tokenId = await starX721Token.getCurTokenId()
+            await starX721Token.mint(user, {from: user})
+            const tokenOwner = await starX721Token.ownerOf(tokenId);
+            assert.equal(tokenOwner, user)
+        })
+
+        it('9st test, get the balance of the tokenId',async()=>{
+            const balanceBefore = await starX721Token.balanceOf(user)
+            await starX721Token.mint(user,{from:user})
+            const balanceAfter = await starX721Token.balanceOf(user)
+            assert.equal(balanceAfter.sub(balanceBefore),"1")
+        })
+    })
+
 })
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,8 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ require("dotenv").config()
+ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   /**
@@ -41,11 +38,11 @@ module.exports = {
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    //
     local: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",  // Any network (default: none)
+     gasPrice: 0x01,     
     },
     rinkeby: {
         provider:function(){
@@ -59,24 +56,27 @@ module.exports = {
         confirmations:2,
         skipDryRun:true
     },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    mainnet: {
+        provider: function() {
+          return new HDWalletProvider(process.env.MAINNET_MNEMONIC, process.env.MAINNET_RPC_URL);
+        },
+        network_id: '1',
+        gas: 7500000,
+        gasPrice: 60000000000,
+        from: process.env.MAINNET_ACCOUNT,
+        timeoutBlocks: 200,
+        confirmations: 2,
+        skipDryRun: false
+      },
   },
-
+  plugins: [
+    'truffle-plugin-verify',
+    'truffle-contract-size', //  truffle run contract-size
+    'solidity-coverage' // truffle run coverage
+  ],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
@@ -87,13 +87,13 @@ module.exports = {
     solc: {
        version: "0.8.7",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+    //    evmVersion: "byzantium"
+      }
     }
   },
 
